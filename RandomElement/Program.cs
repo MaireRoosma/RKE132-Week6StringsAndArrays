27ac2236507bf128722d814f8a54aaa5cb68c74f
Maire Roosma. <roosmamaire@gmail.com>
2024-03-02 15:02:09 +0200

    add Week6StringsAndArrays code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,9 @@
+﻿
+string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "Asian takeaway", "butter chicken" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight i am going to eat {snacks[randomIndex]} .");
+
